c4b6b61d7a8036d2e46b8e86e8dad9aed0ea3c06
Akshay-bhatia22 <dpsvn.aksh06001@gmail.com>
2021-11-05 15:12:34 +0530

    heroku bug fix2

--- NotesApp/NotesApp/settings.py
+++ NotesApp/NotesApp/settings.py
@@ -133,4 +133,4 @@ MEDIA_ROOT=os.path.join(BASE_DIR, 'media').replace('\\','/')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
+ALLOWED_HOSTS=['*']
